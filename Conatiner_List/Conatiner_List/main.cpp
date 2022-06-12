64b4f6146b71e1c010c12aadd8ed4c4281da4754
신보원 <bowo9777@naver.com>
2022-06-12 23:29:28 +0900

    List_숙제 ver0.1

--- Conatiner_List/Conatiner_List/main.cpp
+++ Conatiner_List/Conatiner_List/main.cpp
@@ -13,9 +13,10 @@ struct List
 
 void AddObject(List* _Next,const int& _Value);
 void PrintList(List* _Next);
-
 void Insert(List* _Next,const int& _Value,int _Where);
 void Erase(List* _Next,int _Where );
+
+int Count = 0;
 
 int main(void)
 {
@@ -57,11 +58,88 @@ void PrintList(List* _Next)
 // 이거 아래 두개 숙제
 void Insert(List* _Next, const int& _Value, int _Where)
 {
-	if (_Where == 0)
+	if (Count == 0)
 	{
+		if (_Next->Back != nullptr)
+		{
+			List* Add = new List;
+			Add->Value = _Value;
+			_Next->Back = Add;
+			Add->Back = _Next->Back->Back;
+			_Next = Add;
+		}
+		else
+		{
+			List* Add = new List;
+			Add->Value = _Value;
+			_Next->Back = Add;
+			_Next = Add;
+		}
+	}
+	else if (_Where == 0)
+	{
+		if (_Next->Back != nullptr)
+		{
+			List* Add = new List;
+			Add->Value = _Value;
+			_Next->Back = Add;
+			Add->Back = _Next->Back->Back;
+		}
+		else
+		{
+			List* Add = new List;
+			Add->Value = _Value;
+			_Next->Back = Add;
+		}
+	}
+	else
+	{
+		++Count;
+		Insert(_Next->Back, _Value, --_Where);
+		--Count;
 	}
 }
 
 void Erase(List* _Next, int _Where)
 {
+	if (Count == 0)
+	{
+		if (_Next->Back != nullptr)
+		{
+			List* list_Erase;
+			list_Erase = _Next->Back;
+			delete _Next;
+			_Next = nullptr;
+			_Next = list_Erase;
+		}
+		else
+		{
+			delete _Next;
+			_Next = nullptr;
+		}
+	}
+	else if (_Where == 0)
+	{
+		if (_Next->Back != nullptr)
+		{
+			List* list_Erase ;
+			list_Erase = _Next;
+			_Next = _Next->Back;
+			delete list_Erase;
+			list_Erase = nullptr;
+		}
+		else
+		{
+			List* list_delete;
+			list_delete = _Next;
+			delete list_delete;
+			list_delete = nullptr;
+		}
+	}
+	else
+	{
+		++Count;
+		Erase(_Next->Back, --_Where);
+		--Count;
+	}
 }
