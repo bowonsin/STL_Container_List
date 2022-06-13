16aa74cfee46002711b3f9ca160c86ad3d685d88
a <a>
2022-06-13 12:51:04 +0900

    list Ver0.3 Fucking

--- Conatiner_List/Conatiner_List/main.cpp
+++ Conatiner_List/Conatiner_List/main.cpp
@@ -11,11 +11,15 @@ struct List
 	List* Back;
 };
 
-void AddObject(List* _Next,const int& _Value);
+void push_back(List* _Next,const int& _Value);
 void PrintList(List* _Next);
 
-void Insert(List** _Next,const int& _Value,int _Where, int Count);
+void Insert(List* _Next,const int& _Value,int _Where, int Count);
 void Erase(List** _Next,int _Where , int Count);
+
+void T_Insert(List* _Next, const int& _Value, int _Where);
+void T_Erase(List* _Next, int _Where);
+
 
 int main(void)
 {
@@ -25,17 +29,19 @@ int main(void)
 
 	for (int i = 0; i < 10; ++i)
 		// ** 10회 추가
-		AddObject(NumberList, i * 10 + 10);
+		push_back(NumberList, i * 10 + 10);
 
-	Erase(&NumberList, 4, NULL);
-	Insert(&NumberList, 22, 7,NULL);
+	//Erase(&NumberList, 4, NULL);
+	//Insert(NumberList, 22, 7,NULL);
+	T_Insert(NumberList, 15, 2);
+	T_Erase(NumberList, 2);
 
 	PrintList(NumberList);
 
 	return 0;
 }
 
-void AddObject(List* _Next,const int& _Value)
+void push_back(List* _Next,const int& _Value)
 {
 	if (_Next->Back == nullptr)
 	{
@@ -46,7 +52,7 @@ void AddObject(List* _Next,const int& _Value)
 
 	}
 	else
-		AddObject(_Next->Back, _Value);
+		push_back(_Next->Back, _Value);
 }
 
 void PrintList(List* _Next)
@@ -58,42 +64,42 @@ void PrintList(List* _Next)
 }
 
 // 이거 아래 두개 숙제
-void Insert(List** _Next, const int& _Value, int _Where, int Count)
+void Insert(List* _Next, const int& _Value, int _Where, int Count)
 {
 	if (Count == NULL && _Where == 0)
 	{
-		if ((*_Next)->Back != nullptr)
+		if ((_Next)->Back != nullptr)
 		{
 			List* Add = new List;
 			Add->Value = _Value;
-			Add->Back = (*_Next);
-			(*_Next) = Add;
+			Add->Back = (_Next);
+			(_Next) = Add;
 		}
 		else
 		{
 			List* Add = new List;
 			Add->Value = _Value;
-			Add->Back = (*_Next);
+			Add->Back = (_Next);
 		}
 	}
 	else if (_Where == 0 && Count != NULL)
 	{
-		if ((*_Next)->Back != nullptr)
+		if ((_Next)->Back != nullptr)
 		{
 			List* Add = new List;
 			Add->Value = _Value;
-			Add->Back = (*_Next)->Back->Back;
-			(*_Next)->Back = Add;
+			Add->Back = (_Next)->Back;
+			(_Next)->Back = Add;
 		}
 		else
 		{
 			List* Add = new List;
 			Add->Value = _Value;
-			(*_Next)->Back = Add;
+			(_Next)->Back = Add;
 		}
 	}
 	else
-		Insert( &(*_Next)->Back, _Value, --_Where, ++Count);
+		Insert( (_Next)->Back, _Value, --_Where, ++Count);
 }
 
 void Erase(List** _Next, int _Where, int Count)
@@ -130,4 +136,40 @@ void Erase(List** _Next, int _Where, int Count)
 	}
 	else
 		Erase(&(*_Next)->Back, --_Where, ++Count);
+}
+
+void T_Insert(List* _Next, const int& _Value, int _Where)
+{
+	if (_Where == 1)
+	{
+		List* Temp = new List;
+
+		Temp->Value = _Value;
+		Temp->Back = _Next->Back;
+
+		_Next->Back = Temp;
+	}
+	else
+		T_Insert(_Next->Back, _Value, --_Where);
+}
+
+List* Front = nullptr;
+
+void T_Erase(List* _Next, int _Where)
+{
+	if (_Where == 1)
+	{
+		//auto Temp = _Next;
+		//_Next = _Next->Back;
+
+		Front->Back = _Next->Back;
+
+		delete _Next;
+		_Next = nullptr;
+	}
+	else
+	{
+		Front = _Next;
+		T_Erase(_Next->Back, --_Where);
+	}
 }
