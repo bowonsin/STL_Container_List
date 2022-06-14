873a7968d998b1a33b34ffc435fecd91fccfbe1c
a <a>
2022-06-14 12:47:32 +0900

    List Ver0.4

--- Conatiner_List/Conatiner_List/main.cpp
+++ Conatiner_List/Conatiner_List/main.cpp
@@ -7,23 +7,28 @@ using namespace std;
 
 struct List
 {
+	List* Front;
 	int Value;
 	List* Back;
 };
 
-void push_back(List* _Next,const int& _Value);
-void PrintList(List* _Next);
+void push_back(List* _Current,const int& _Value);
+void PrintList(List* _Current);
 
-void Insert(List* _Next,const int& _Value,int _Where, int Count);
-void Erase(List** _Next,int _Where , int Count);
+void Insert(List* _Current,const int& _Value,int _Where, int Count);
+void Erase(List** _Current,int _Where , int Count);
 
-void T_Insert(List* _Next, const int& _Value, int _Where);
-void T_Erase(List* _Next, int _Where);
+void T_Insert(List* _Current, const int& _Value, int _Where);
+void T_Erase(List* _Current, int _Where);
+void T_Pop_Back(List* _Current);
+
+List* End = nullptr;
 
 
 int main(void)
 {
 	List* NumberList = new List;
+	NumberList->Front = nullptr;
 	NumberList->Value = 0;
 	NumberList->Back = nullptr;
 
@@ -36,140 +41,166 @@ int main(void)
 	T_Insert(NumberList, 15, 2);
 	T_Erase(NumberList, 2);
 
+	T_Pop_Back(NumberList);
 	PrintList(NumberList);
 
 	return 0;
 }
 
-void push_back(List* _Next,const int& _Value)
+void push_back(List* _Current,const int& _Value)
 {
-	if (_Next->Back == nullptr)
+	if (_Current->Back == nullptr)
 	{
 		List* Temp = new List;
+		
+		Temp->Front = _Current;
 		Temp->Value = _Value;
 		Temp->Back = nullptr;
-		_Next->Back = Temp;
+		_Current->Back = Temp;
 
+		End = Temp;
 	}
 	else
-		push_back(_Next->Back, _Value);
+		push_back(_Current->Back, _Value);
 }
 
-void PrintList(List* _Next)
+void PrintList(List* _Current)
 {
-	cout << _Next->Value << endl;
+	cout << _Current->Value << endl;
 		
-	if (_Next->Back != nullptr)
-		PrintList(_Next->Back);
+	if (_Current->Back != nullptr)
+		PrintList(_Current->Back);
 }
 
-// 이거 아래 두개 숙제
-void Insert(List* _Next, const int& _Value, int _Where, int Count)
+// 이거 아래 두개 숙제 ->List에 Front 가 없을 떄 사용
+void Insert(List* _Current, const int& _Value, int _Where, int Count)
 {
 	if (Count == NULL && _Where == 0)
 	{
-		if ((_Next)->Back != nullptr)
+		if ((_Current)->Back != nullptr)
 		{
 			List* Add = new List;
 			Add->Value = _Value;
-			Add->Back = (_Next);
-			(_Next) = Add;
+			Add->Back = (_Current);
+			(_Current) = Add;
 		}
 		else
 		{
 			List* Add = new List;
 			Add->Value = _Value;
-			Add->Back = (_Next);
+			Add->Back = (_Current);
 		}
 	}
 	else if (_Where == 0 && Count != NULL)
 	{
-		if ((_Next)->Back != nullptr)
+		if ((_Current)->Back != nullptr)
 		{
 			List* Add = new List;
 			Add->Value = _Value;
-			Add->Back = (_Next)->Back;
-			(_Next)->Back = Add;
+			Add->Back = (_Current)->Back;
+			(_Current)->Back = Add;
 		}
 		else
 		{
 			List* Add = new List;
 			Add->Value = _Value;
-			(_Next)->Back = Add;
+			(_Current)->Back = Add;
 		}
 	}
 	else
-		Insert( (_Next)->Back, _Value, --_Where, ++Count);
+		Insert( (_Current)->Back, _Value, --_Where, ++Count);
 }
 
-void Erase(List** _Next, int _Where, int Count)
+void Erase(List** _Current, int _Where, int Count)
 {
 	if (Count == NULL && _Where == 0)
 	{
-		if ((*_Next)->Back != nullptr)
+		if ((*_Current)->Back != nullptr)
 		{
-			List* Add = (*_Next)->Back;
-			delete (*_Next);
-			(*_Next) = nullptr;
-			(*_Next) = Add;
+			List* Add = (*_Current)->Back;
+			delete (*_Current);
+			(*_Current) = nullptr;
+			(*_Current) = Add;
 		}
 		else
 		{
-			delete (*_Next);
-			(*_Next) = nullptr;
+			delete (*_Current);
+			(*_Current) = nullptr;
 		}
 	}
 	else if (Count != NULL && _Where == 0)
 	{
-		if ((*_Next)->Back != nullptr)
+		if ((*_Current)->Back != nullptr)
 		{
-			List* Add = (*_Next)->Back;
-			delete (*_Next);
-			(*_Next) = nullptr;
-			(*_Next) = Add;
+			List* Add = (*_Current)->Back;
+			delete (*_Current);
+			(*_Current) = nullptr;
+			(*_Current) = Add;
 		}
 		else
 		{
-			delete (*_Next);
-			(*_Next) = nullptr;
+			delete (*_Current);
+			(*_Current) = nullptr;
 		}
 	}
 	else
-		Erase(&(*_Next)->Back, --_Where, ++Count);
+		Erase(&(*_Current)->Back, --_Where, ++Count);
 }
 
-void T_Insert(List* _Next, const int& _Value, int _Where)
+void T_Insert(List* _Current, const int& _Value, int _Where)
 {
 	if (_Where == 1)
 	{
 		List* Temp = new List;
 
+		Temp->Front = _Current;
 		Temp->Value = _Value;
-		Temp->Back = _Next->Back;
+		Temp->Back = _Current->Back;
 
-		_Next->Back = Temp;
+		_Current->Back = Temp;
 	}
 	else
-		T_Insert(_Next->Back, _Value, --_Where);
+		T_Insert(_Current->Back, _Value, --_Where);
 }
 
 List* Front = nullptr;
 
-void T_Erase(List* _Next, int _Where)
+void T_Erase(List* _Current, int _Where)
 {
 	if (_Where == 1)
 	{
-		//auto Temp = _Next;
-		//_Next = _Next->Back;
+		//auto Temp = _Current;
+		//_Current = _Current->Back;
 
-		Front->Back = _Next->Back;
+		//Front->Back = _Current->Back;
 
-		delete _Next;
-		_Next = nullptr;
+		List* del = _Current;
+
+		_Current->Front->Back = _Current->Back;
+		_Current->Back->Front = _Current->Front ;
+
+		delete del;
+		del = nullptr;
+
+		
 	}
 	else
-	{
-		Front = _Next;
-		T_Erase(_Next->Back, --_Where);
-	}
+		T_Erase(_Current->Back, --_Where);
 }
+
+void T_Pop_Back(List* _Current)
+{
+	if (_Current->Back == nullptr)
+	{
+		End = _Current->Front;
+
+		_Current->Front->Back = nullptr;
+
+		delete _Current;
+		_Current = nullptr;
+	}
+	else
+		T_Pop_Back(_Current->Back);
+}
+
+
